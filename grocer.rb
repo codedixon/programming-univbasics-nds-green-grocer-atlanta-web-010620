37f2775275e5b5a5ad8b2a4f70679f5f3d660989
Rashad Dixon <dixoncodes@gmail.com>
2019-12-22 23:01:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,11 @@
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+ items = {}
+ i = 0
+ while i < collection.length
+ items[collection[i][:item]] = collection[i]
+ i += 1
+ end
+ return items[name]
 end
 
 def consolidate_cart(cart)
@@ -9,12 +13,44 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  full_cart = []
+  
+  i = 0
+  while i < cart.length
+  full_cart[i] = cart[i]
+  if full_cart[i][:count]
+   full_cart[i][:count] += 1
+  else 
+   full_cart[i][:count] = 1
+    end
+    i += 1
+  end
+  puts cart
+  return full_cart
 end
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
-  #
+  coupons_hash = {}
+  #count_hash = consolidate_cart(cart)
+  j = 0
+  while j < coupons.length
+  coupons_hash[coupons[j][:item]] = coupons[j][:num]
+  j += 1
+end
+#k = 0
+ #while k < cart.length
+  #count_hash[cart[k][:item]] = cart[k][:count]
+  #k += 1
+#end
+  #puts count_hash
+  i = 0
+  #while i < cart.length
+  #if cart[i][:count] < cart[i][:num]
+  #i += 1
+#end
   # REMEMBER: This method **should** update cart
+  return cart
 end
 
 def apply_clearance(cart)
