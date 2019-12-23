0d6da16fb5005133520dc8121bfb12a84a81a5de
Rashad Dixon <dixoncodes@gmail.com>
2019-12-23 00:34:13 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -35,7 +35,7 @@ def apply_coupons(cart, coupons)
   item_name = find_item_by_name_in_collection(cart[i][:item], cart)
   while j < coupons.length
   if coupons[j][item_name] && cart[i][:count] >= coupons[j][:num]
-    cart
+    cart << 
   # REMEMBER: This method **should** update cart
   
   return cart
