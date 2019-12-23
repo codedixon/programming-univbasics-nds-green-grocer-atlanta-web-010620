bf873435ad203078b25388ab58df54c28fe13b5c
Rashad Dixon <dixoncodes@gmail.com>
2019-12-23 00:10:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -31,7 +31,7 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   coupons_hash = {}
   count_hash = consolidate_cart(cart)
-  puts count_hash
+  puts count_hash[0]
   # REMEMBER: This method **should** update cart
   
   return cart
